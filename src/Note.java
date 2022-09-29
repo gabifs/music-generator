579beb9644291b8ff55565df997b858dd7722e1e
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-09-29 19:22:18 -0300

    remove octaveManager, and add Note

---
+++ src/Note.java
@@ -0,0 +1,18 @@
+public class Note {
+  static final int DEFAULT_OCTAVE = 5;
+
+  int octave = DEFAULT_OCTAVE;
+
+  public String runNote(String note){
+    return note + octave;
+  }
+
+  public void increaseOctave(int num){
+    octave += 1;
+    if(octave > 9) resetOctave();
+  }
+
+  public void resetOctave(){
+    octave = DEFAULT_OCTAVE;
+  }
+}
